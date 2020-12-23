a964b29227739bf0e79aa930f67ab730587cf41b
itsumi33 <770357906@qq.com>
2020-12-23 19:55:39 +0800

    测试

--- config/index.js
+++ config/index.js
@@ -10,15 +10,6 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      "/api":{
-        target:"http://localhost:3000",
-        changeOrigin:true,
-        pathRewrite:{
-          "^/api":"http://localhost:3000"
-        }
-      }
-    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
